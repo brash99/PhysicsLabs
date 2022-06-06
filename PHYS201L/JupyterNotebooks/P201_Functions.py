146b9dd398732280d442574a58c20945d225c960
physlabpcse <brash99@gmail.com>
2022-06-06 11:49:32 -0400

    Add weighted_average function and new notebook

--- PHYS201L/JupyterNotebooks/P201_Functions.py
+++ PHYS201L/JupyterNotebooks/P201_Functions.py
@@ -388,6 +388,39 @@ def weighted_average(x,deltax,etype):
     
     return xbar,delta_xbar
 
+def weighted_average_full(x,deltax,etype):
+    
+    if (etype == 1):
+        w = 1/deltax
+    else:
+        w = 1/deltax**2
+    
+    # calculate the statistically weighted average of density
+    sq_sum1 = 0.0
+    sq_sum2 = 0.0
+    sq_sum3 = 0.0
+    sq_sum4 = 0.0
+    sq_sum5 = 0.0
+    
+    for i in range(len(x)):
+        sq_sum1 = sq_sum1 + w[i]*x[i]
+        sq_sum2 = sq_sum2 + w[i]
+        sq_sum3 = sq_sum3 + w[i]*x[i]**2
+        sq_sum4 = sq_sum4 + w[i]
+        sq_sum5 = sq_sum5 + w[i]**2
+                   
+    xbar = sq_sum1/sq_sum2
+    delta_xbar = np.sqrt((np.abs(sq_sum3/sq_sum2 - xbar**2))*
+                             sq_sum5/(sq_sum4**2-sq_sum5))
+    
+    neff = sq_sum4**2/sq_sum5
+    print("Effective N_dof = ",neff)
+    
+    factor = np.sqrt((neff-1.0)/(len(x)-1))
+    print("Correction factor = ",factor)
+    
+    return xbar,delta_xbar,neff,factor
+
 
 def set_dark_mode(dark_mode = True):
     if (dark_mode):
